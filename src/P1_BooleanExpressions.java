9f6d77d803728dcd43684c65a98e91792a6b5081
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-04-19 12:01:25 -0500

    b

--- src/P1_BooleanExpressions.java
+++ src/P1_BooleanExpressions.java
@@ -1,36 +1,36 @@
 /*
-	ISYS 320
-	Name(s):
-	Date: 
+ISYS 320 DONE DONE DONE DONE
+	Name(s): Dave Rapp
+	Date: 4-18-18
 */
 
 /*
  Your expression predictions:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. True
+ 2. True
+ 3. False
+ 4. True
  
- 5.
- 6.
- 7.
- 8.
+ 5. True
+ 6. False
+ 7. True
+ 8. True
    
  */
 
 /*
  Were you correct? Explain if you were not (what did you learn?)
  
- 1.
- 2.
- 3. 
- 4.
+ 1. Yes
+ 2. Yes
+ 3. Yes
+ 4. Yes
  
- 5.
- 6.
- 7.
- 8.
+ 5. Yes
+ 6. Yes
+ 7. Yes
+ 8. Yes
 
  */
 public class P1_BooleanExpressions {
@@ -41,7 +41,7 @@ public class P1_BooleanExpressions {
 		int z = 32; 
 		boolean b = false; 
 		
-		System.out.println("REPLACE THIS STRING WITH YOUR BOOLEAN EXPRESSION");
+		System.out.println(!b );
 	}
 
 }
--- src/P3_Consecutive.java
+++ src/P3_Consecutive.java
@@ -1,19 +1,36 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Dave Rapp
+	Date: 4-18-18
 */
+
+/*    public static boolean consecutive(int num1, int num2, int num3) {
+		int small=Math.min(num1, Math.min(num2,num3));
+		return (num1+num2+num3 ==small+(small+1)+ (small+2)); }
+		
+---------------------------------------------------------------------------------
+		
+		public boolean consecutive(int a, int b, int c) {
+    int max = Math.max(a, Math.max(b, c));
+    int min = Math.min(a, Math.min(b, c));
+    int mid = a + b + c - max - min;
+    
+    return max == mid + 1 && mid == min + 1;
+}
+ --------------------------------------------------------------------------------
+		*/
 
 import java.util.Scanner;
 
 public class P3_Consecutive {
+	
 
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
 		System.out.println("This program calculates if three input values are consecutive.");
 		
 		String doItAgainResponse = "";
-		
+
 		do {
 			System.out.print("Enter value 1: ");
 			int num1 = console.nextInt();
@@ -21,18 +38,22 @@ public class P3_Consecutive {
 			int num2 = console.nextInt();
 			System.out.print("Enter value 3: ");
 			int num3 = console.nextInt();
+			
+			
 		
 			System.out.printf( "a: %d, b: %d, c: %d -> ", num1, num2, num3);
 			
-
 			
+
 			System.out.print("Would you like to do it again (enter 'n' to quit)? ");
 			doItAgainResponse = console.next();
+			
+				
 		} while( !doItAgainResponse.equalsIgnoreCase("n"));
+		
+		
 		
 		System.out.println("Thanks!");
 	}
 	
-
-
 }
